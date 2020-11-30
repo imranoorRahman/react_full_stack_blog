e16c6ac6f20ba8ed612dd70f3649843e1c48ccbc
imran <mrimranoor@gmail.com>
2020-11-30 23:39:54 +0000

    Added page renderer, switch, route

--- my-react-blog/src/App.js
+++ my-react-blog/src/App.js
@@ -6,11 +6,19 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
+
+import PageRenderer from "./page-renderer";
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Navigation />
+        <Switch>
+          <Route path="/:page" component={PageRenderer} />
+          <Route path="/" render={() => <Redirect to="/home" />} />
+          <Route component={() => 404} />
+        </Switch>
       </div>
     </Router>
   );
