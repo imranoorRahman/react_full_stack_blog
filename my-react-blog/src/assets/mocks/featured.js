2ab6c698fb8e87f54b8c69852a13bc0bd9f37737
imran <mrimranoor@gmail.com>
2020-12-03 23:55:41 +0000

    Added mock data, imgs and masonry

---
+++ my-react-blog/src/assets/mocks/featured.js
@@ -0,0 +1,53 @@
+import moment from "moment";
+
+export default [
+  {
+    title: "Learning to code",
+    date: moment().format("DD-MMMM-YYYY"),
+    categories: ["Tech Culture", "Tech News"],
+    link: "#",
+    image: "img (1).jpg",
+  },
+  {
+    title: "Learning to code",
+    date: moment().format("DD-MMMM-YYYY"),
+    categories: ["Tech Culture", "Tech News"],
+    link: "#",
+    image: "img (1).jpg",
+  },
+  {
+    title: "Learning to code",
+    date: moment().format("DD-MMMM-YYYY"),
+    categories: ["Tech Culture", "Tech News"],
+    link: "#",
+    image: "img (1).jpg",
+  },
+  {
+    title: "Learning to code",
+    date: moment().format("DD-MMMM-YYYY"),
+    categories: ["Tech Culture", "Tech News"],
+    link: "#",
+    image: "img (1).jpg",
+  },
+  {
+    title: "Learning to code",
+    date: moment().format("DD-MMMM-YYYY"),
+    categories: ["Tech Culture", "Tech News"],
+    link: "#",
+    image: "img (1).jpg",
+  },
+  {
+    title: "Learning to code",
+    date: moment().format("DD-MMMM-YYYY"),
+    categories: ["Tech Culture", "Tech News"],
+    link: "#",
+    image: "img (1).jpg",
+  },
+  {
+    title: "Learning to code",
+    date: moment().format("DD-MMMM-YYYY"),
+    categories: ["Tech Culture", "Tech News"],
+    link: "#",
+    image: "img (1).jpg",
+  },
+];
---
+++ my-react-blog/src/assets/mocks/trending.js
@@ -0,0 +1,25 @@
+import moment from "moment";
+
+export default [
+  {
+    title: "Is Python the real deal?",
+    date: moment().format("DD-MMMM-YYYY"),
+    categories: ["Tech Culture", "Tech News"],
+    link: "#",
+    image: "img (2).jpg",
+  },
+  {
+    title: "Is Python the real deal?",
+    date: moment().format("DD-MMMM-YYYY"),
+    categories: ["Tech Culture", "Tech News"],
+    link: "#",
+    image: "img (2).jpg",
+  },
+  {
+    title: "Is Python the real deal?",
+    date: moment().format("DD-MMMM-YYYY"),
+    categories: ["Tech Culture", "Tech News"],
+    link: "#",
+    image: "img (2).jpg",
+  },
+];
