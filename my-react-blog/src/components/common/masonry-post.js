6000c31ee72edcff2d0d7d1d2cf5b02eb0af531e
imran <mrimranoor@gmail.com>
2020-12-03 23:58:59 +0000

    populated masonry-post and post-masonry scss files

--- my-react-blog/src/components/common/masonry-post.js
+++ my-react-blog/src/components/common/masonry-post.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 export default function MasonryPost({ post, tagsOnTop }) {
-    const style = {backgroundImage: `url("${require(`../../assets/images/${post.image}`)}")`};
-  return <a className="masonry-post overlay" style={style} href={post.link}>
-      
-  </a>;
+  const style = {
+    backgroundImage: `url("${require(`../../assets/images/${post.image}`)}")`,
+  };
+  return (
+    <a className="masonry-post overlay" style={style} href={post.link}>
+      <div className="image-text"></div>
+    </a>
+  );
 }
